18db2a91d6b95c810917e20a3398a7adf187c405
JS_Shan <7931472+js_shan@user.noreply.gitee.com>
2024-04-05 04:57:17 +0800

    Site updated: 2024-04-05 04:57:16

--- js/search.js
+++ js/search.js
@@ -1,78 +1,80 @@
-// Global searchConfig
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  const input = document.querySelector('.search-input');
-  const container = document.querySelector('.search-result-container');
-
-  const localSearch = new LocalSearch({
-    path             : searchConfig.path,
-    top_n_per_article: searchConfig.top_n_per_article,
-    unescape         : searchConfig.unescape
-  });
-
-  if (searchConfig.preload) {
-    // preload the search data when the page loads
-    console.log("loading page");
-    localSearch.fetchData();
-  }
-
-  function openSearchPopup() {
-    document.querySelector('.search-popup').classList.add('search-activate');
-    if (!localSearch.isfetched) {
-      localSearch.fetchData();
-    } 
-  }
-
-  function closeSearchPopup() {
-    document.querySelector('.search-popup').classList.remove('search-activate');
-    // refresh search box
-    input.value = '';
-    container.innerHTML = `<div class="search-result-message" ></div>`;
-  }
-
-  // open search box
-  document.querySelector('.search-btn').addEventListener('click', openSearchPopup);
-
-  // close search box
-  document.querySelector('.search-popup-overlay').addEventListener('click', closeSearchPopup);
-  document.querySelector('.search-close-btn').addEventListener('click', closeSearchPopup);
-
-  function displaySearchResult() {
-    if (!localSearch.isfetched) return;
-    const searchText = input.value.trim().toLowerCase();
-    const keywords = searchText.split(/[-\s]+/);
-    if (searchText.length > 0) {
-      resultItems = localSearch.getResultItems(keywords);
-    }
-
-    if (keywords.length === 1 && keywords[0] === '') {
-      // no input
-      container.innerHTML = `<div class="search-result-message" ></div>`
-    } else if (resultItems.length === 0) {
-      // no result
-      container.innerHTML = `<div class="search-result-message" >No result found</div>`;
+// 参考 https://chaooo.github.io/article/20161109.html
+let searchData;
+function loadData(arg) {
+    if (!searchData) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '/content.json', true);
+        xhr.onload = function() {
+            if (this.status >= 200 && this.status < 300) {
+                const res = JSON.parse(this.response || this.responseText);
+                searchData = res instanceof Array ? res : res.posts;
+                searchkey(arg);
+            } else {
+                console.error(statusText);
+            }
+        };
+        xhr.onerror = function() {
+            console.error(statusText);
+        };
+        xhr.send();
     } else {
-      // display result(s)
-      container.innerHTML = `
-      <div class="search-result-message">${resultItems.length} result(s) found</div>
-      <ul class="search-result-list">${resultItems.map(result => result.item).join('<div class="h-line-secondary"></div>')}
-      </ul>`;
+        searchkey(arg);
     }
+}
 
-  };
+function searchkey(keyword) {
+    keyword.forEach(word => {
+        searchData.forEach(post => {
+            const rend = {};
+            const reg = new RegExp(word, 'g');
+            let flag = false;
+            if (post.title.search(reg) !== -1) {
+                rend.title = post.title.replace(reg, `<span class="keyword">${word}</span>`);
+                flag = true;
+            }
+            const textpos = post.text.search(reg);
+            if (textpos !== -1) {
+                rend.text = `…${post.text.substring(textpos, textpos + 18)}…`;
+                rend.text = rend.text.replace(reg, `<span class="keyword">${word}</span>`);
+                flag = true;
+            }
+            if (flag) {
+                rend.title = !rend.title ? post.title : rend.title;
+                rend.text = !rend.text ? post.text : rend.text;
+                rend.href = `/${post.path}`;
+                render(rend);
+            }
+        });
+    });
+}
 
-  if (searchConfig.trigger == 'auto') {
-    // whenever there is input, update search result
-    input.addEventListener('input', displaySearchResult);
-  } else {
-    // update search result when press "enter"
-    input.addEventListener('keypress', event => {
-      if (event.key === 'Enter') {
-        displaySearchResult();
-      }
-    })
-  }
-  window.addEventListener('search:loaded', displaySearchResult);
-});
-  
+function render(data) {
+    const ele = document.createElement('div');
+    ele.className = 'search-result';
+    ele.innerHTML = `<a href=${data.href}><h3>${data.title}</h3>
+    <span class="content">${data.text}<br>　</span></a>`;
+    document.getElementsByClassName('search-body')[0].appendChild(ele);
+}
+
+// 主程序
+const key = decodeURI(location.search.split('?q=')[1]);
+if (key !== undefined && key !== 'undefined') {
+    document.getElementsByClassName('search-input')[0].value = key;
+    loadData(format(key));
+    document.getElementById('nexmoe-search-space').style.display = 'flex';
+}
+
+// 事件
+function sclose() {
+    document.getElementById('nexmoe-search-space').style.display = 'none';
+}
+
+function sinput() {
+    document.getElementsByClassName('search-body')[0].innerHTML = '';
+    loadData(format(document.getElementsByClassName('search-input')[0].value));
+}
+
+// 搜索词格式化
+function format(word) {
+    return word.replace(/[ ]/g, '').split(' ');
+}
